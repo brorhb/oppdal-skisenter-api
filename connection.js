29293ca3050981df17a8893f86e162b32304440c
brorhb <bror@hey.com>
2020-11-14 10:45:44 +0100

    formatting

--- connection.js
+++ connection.js
@@ -10,19 +10,18 @@ const config = {
 module.exports = function handleDisconnect() {
   connection = mysql.createConnection(config)
 
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
+  connection.connect(function(err) {
+    if (err) {
+      console.log('error when connecting to db:', err)
+      setTimeout(handleDisconnect, 2000)
+    }
+  })
   connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
+    console.log('db error', err)
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect()
+    } else {
+      throw err
     }
   });
 }
